757b044462a8b27ad64258e58a351beca8b8df9b
User <Maksim002>
2020-03-25 17:21:48 +0600

    new

--- app/src/main/java/com/example/newspiner/MainActivity.java
+++ app/src/main/java/com/example/newspiner/MainActivity.java
@@ -16,10 +16,6 @@ import android.widget.Spinner;
 
 import com.google.android.material.textfield.TextInputLayout;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import fr.ganfra.materialspinner.MaterialSpinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,19 +23,12 @@ public class MainActivity extends AppCompatActivity {
     private TextInputLayout text;
     private Button button;
 
-    private String[] cats;
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cats = new String[]{"aaa", "sss", "ddd", "fff"};
-
-        text = findViewById(R.id.text_my_editText1);
         textView = findViewById(R.id.spinner1);
-
 
         String[] ITEMS = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, ITEMS);
